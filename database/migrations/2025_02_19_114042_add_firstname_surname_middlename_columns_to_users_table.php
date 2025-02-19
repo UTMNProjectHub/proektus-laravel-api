81206b33a3f8c66298aa86b0f845c13dc75c6679
flipzky1337 <rorytheloli1337@gmail.com>
2025-02-19 16:51:12 +0500

    add full name to user table

---
+++ database/migrations/2025_02_19_114042_add_firstname_surname_middlename_columns_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('firstname');
+            $table->string('surname');
+            $table->string('middlename');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropIfExists('firstname');
+            $table->dropIfExists('surname');
+            $table->dropIfExists('middlename');
+        });
+    }
+};
