cb0fd5af69d4952961b70d7532be7f138eb8b18e
flipzky1337 <rorytheloli1337@gmail.com>
2025-02-21 22:15:08 +0500

    cors-csrf-api fixes for local development

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -7,19 +7,29 @@ use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
 
 class AuthenticatedSessionController extends Controller
 {
     /**
      * Handle an incoming authentication request.
+     * @throws ValidationException
      */
-    public function store(LoginRequest $request): Response
+    public function store(LoginRequest $request): \Illuminate\Http\JsonResponse
     {
         $request->authenticate();
 
+        if (! Auth::check()) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
         $request->session()->regenerate();
 
-        return response()->noContent();
+        $user = Auth::user();
+
+        return response()->json([
+            'user' => $user,
+        ], 200);
     }
 
     /**
--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000'), 'http://172.30.74.94:5173', 'http://localhost:5173'],
 
     'allowed_origins_patterns' => [],
 
