ce4b65bd0c1c06b682751058809ac9823c7eb8aa
flipzky1337 <rorytheloli1337@gmail.com>
2025-03-08 21:34:14 +0500

    L try at making file uploading to S3

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,25 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return new UserResource($request->user());
 });
 
-Route::group((array)'/admin', function () {
-    Route::group((array)'/users', function () {
-        Route::get('/', 'UserController@index');
-        Route::get('/{user}', 'UserController@show');
-//        Route::put('/{user}', 'UserController@update');
-        Route::delete('/{user}', 'UserController@destroy');
+//Route::group((array)'/admin', function () {
+//    Route::group((array)'/users', function () {
+//        Route::get('/', 'UserController@index');
+//        Route::get('/{user}', 'UserController@show');
+////        Route::put('/{user}', 'UserController@update');
+//        Route::delete('/{user}', 'UserController@destroy');
+//    });
+//})->middleware(['auth:sanctum', 'role:admin']);
+
+Route::prefix('/admin')->group(function () {
+    Route::controller(\App\Http\Controllers\User\UserController::class)->group(function () {
+        Route::get('/users/', 'index');
+        Route::get('/users/{user}', 'show');
+        Route::delete('users/{user}', 'destroy');
     });
 })->middleware(['auth:sanctum', 'role:admin']);
+
+Route::prefix('/project')->group(function () {
+    Route::controller(\App\Http\Controllers\Project\ProjectFileController::class)->group(function () {
+        Route::post('/file/upload', 'upload');
+    });
+});
